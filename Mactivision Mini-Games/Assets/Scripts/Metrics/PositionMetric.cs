aaeadbcfcbc5643c365f80fc246e1e19b63f49a6
Cmon16 <36305006+Cmon16@users.noreply.github.com>
2021-02-07 21:18:44 -0500

    Update PositionMetric.cs

--- Mactivision Mini-Games/Assets/Scripts/Metrics/PositionMetric.cs
+++ Mactivision Mini-Games/Assets/Scripts/Metrics/PositionMetric.cs
@@ -7,7 +7,6 @@ public class PositionMetric : AbstractMetric<PositionEvent> {
     public override JObject getJSON() {
         JObject json = new JObject();
 
-        // all json data from this metric should be stored under "memoryChoice"
         json["currentValue"] = JToken.FromObject(this.currentValue);
         json["valueChange"] = JToken.FromObject(this.valueChange);
         json["initialValue"] = JToken.FromObject(this.reasonIndex);
