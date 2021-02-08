69dba3294ff7ff974a7f2eeaa6360b06691a4232
David Hospital <david.hospital27@gmail.com>
2021-02-07 23:11:19 -0500

    fix typo

--- Mactivision Mini-Games/Assets/Scripts/Metrics/EditModeTests/TestLinearVariableMetric.cs
+++ Mactivision Mini-Games/Assets/Scripts/Metrics/EditModeTests/TestLinearVariableMetric.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
-using Newtonsoft.Json.Ling;
+using Newtonsoft.Json.Linq;
 
 public class TestLinearVariableMetric
 {
