c32618c6debef43b5c4efe8fdff4639ba52f6d9a
David Hospital <david.hospital27@gmail.com>
2021-02-21 21:28:58 -0500

    fix client and server paths

--- server/client.py
+++ server/client.py
@@ -1,6 +1,11 @@
+import os
+import sys
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 if __name__ == '__main__':
+    path = os.path.dirname(sys.argv[0]) or '.'
+    os.chdir(path)
+
     address = ('', 3000)
     httpd = HTTPServer(address, SimpleHTTPRequestHandler)
     print("Client running on port 3000")
